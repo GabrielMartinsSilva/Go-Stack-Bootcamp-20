532aadaaf15104319b285db77441e64c6a13719b
Gabriel <gamartins77@gmail.com>
2020-06-10 23:05:43 -0300

    Testando as rotas com Insomnia

--- conceitos-dev/backend/src/index.js
+++ conceitos-dev/backend/src/index.js
@@ -1,40 +1,38 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/projects', (request, response) => {
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-  ]);
+app.use(express.json());
+
+app.get("/projects", (request, response) => {
+  const { tittle, owner } = request.query;
+
+  console.log(tittle);
+  console.log(owner);
+
+  return response.json(["Projeto 1", "Projeto 2"]);
 });
 
-app.post('/projects', (request, response) => {
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+app.post("/projects", (request, response) => {
+  const { title, owner } = request.body;
+
+  console.log(title);
+  console.log(owner);
+
+  return response.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
 });
 
-app.put('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+app.put("/projects/:id", (request, response) => {
+  const { id } = request.params;
+  console.log(id);
+
+  return response.json(["Projeto 4", "Projeto 2", "Projeto 3"]);
 });
 
-app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+app.delete("/projects/:id", (request, response) => {
+  return response.json(["Projeto 4", "Projeto 2", "Projeto 3"]);
 });
-
-
 
 app.listen(3333, () => {
-  console.log('✔ Back-end started!')
+  console.log("✔ Back-end started!");
 });
